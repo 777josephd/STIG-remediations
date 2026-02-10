357fd15fe0c16f2e8b06477afb39ca307301ec55
JAD <116841363+777josephd@users.noreply.github.com>
2026-02-10 03:33:30 -0800

    Create STIG-ID-WN11-CC-000110.ps1

---
+++ DISA Microsoft Windows 11 STIG v2r5/STIG-ID-WN11-CC-000110.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    Printing over HTTP must be prevented.
+
+.NOTES
+    Author          : Joseph De Santiago
+    LinkedIn        : linkedin.com/in/joseph-d-0875a324b/
+    GitHub          : github.com/777josephd
+    Date Created    : 2026-02-10
+    Last Modified   : 2026-02-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000110
+
+.TESTED ON
+    Date(s) Tested  : 2026-02-10
+    Tested By       : Joseph De Santiago
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1.26100.7462
+
+.USAGE
+    Download the script and execute.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN11-CC-000110.ps1 
+#>
+
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers" -Force | Out-Null
+New-ItemProperty `
+  -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers" `
+  -Name "DisableHTTPPrinting" `
+  -PropertyType DWord `
+  -Value 1 `
+  -Force
